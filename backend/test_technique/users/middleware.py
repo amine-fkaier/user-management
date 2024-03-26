1b1c9593879a9102e538d37a2b42d09d7d70f6fe
Amine <aminefkaier@gmail.com>
2024-03-26 01:26:03 +0100

    fix some issues

--- backend/test_technique/users/middleware.py
+++ backend/test_technique/users/middleware.py
@@ -6,13 +6,11 @@ class CorsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Handle preflight requests
         if request.method == 'OPTIONS':
             response = HttpResponse()
         else:
             response = self.get_response(request)
 
-        # Set CORS headers
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
         response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
